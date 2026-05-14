845f478911cbff1f3d463b97bf09d33e949d4ad6
Aditya3984 <75792170+Aditya3984@users.noreply.github.com>
2026-05-14 18:57:05 +0530

    Fix image paths and update slide titles in about.js

--- about.js
+++ about.js
@@ -148,7 +148,7 @@ function fineArtsImages(containerId, startIndex) {
   container.innerHTML = '';
   const slides = [
     { title: 'Watercolor Study',   image: "images/finearts/FA1.jpeg" },
-    { title: 'Charcoal Sketch',    image: "images/fineart/FA15.jpeg" },
+    { title: 'Charcoal Sketch',    image: "images/finearts/FA15.jpeg" },
     { title: 'Oil Painting',       image: "images/finearts/FA19.jpeg" },
     { title: 'Figure Drawing',     image: "images/finearts/FA22.jpeg" },
     { title: 'Portrait Study',     image: "images/finearts/FA29.jpeg" },
@@ -218,12 +218,10 @@ function comicImages(containerId, startIndex) {
   container.innerHTML = '';
   const slides = [
     { title: 'Holika Dahan-1',  image: "images/pictopia/Tape a Tale/Holika_Dahan_page1-1.png" },
-    { title: 'Holika Dahan-2',       image: "images/pictopia/Tape a Tale/Holika_Dahan_page2-1.png" },
-    { title: 'Archi',      image: "images/pictopia/Original Stories/ARCHI(Episode-1)Part 1.png" },
-    { title: 'Archi',     image: "images/pictopia/Original Stories/ARCHI(Episode-1)Part 2.png" },
-    { title: 'Archi',  image: "images/pictopia/Original Stories/ARCHI-Episode-2 (Part 1).png" },
-    
-    
+    { title: 'Holika Dahan-2',  image: "images/pictopia/Tape a Tale/Holika_Dahan_page2-1.png" },
+    { title: 'Archi',           image: "images/pictopia/Original Stories/ARCHI(Episode-1)Part 1.png" },
+    { title: 'Archi',           image: "images/pictopia/Original Stories/ARCHI(Episode-1)Part 2.png" },
+    { title: 'Archi',           image: "images/pictopia/Original Stories/ARCHI-Episode 2 (Part 1).png" }, 
   ];
 
   let currentSlide = startIndex || 0;
